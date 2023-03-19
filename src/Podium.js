b5eec6a986f6373ad9f9433ffb72149262bc7f6d
Daniel Andreev <andreevdan@gmail.com>
2023-03-19 17:53:29 -0400

    done ranking

--- src/Podium.js
+++ src/Podium.js
@@ -9,17 +9,17 @@ const Podium = ({ data }) => {
     <div className="podium">
       <div className="podium-item gold">
         <h3>1st</h3>
-        <img src={`path/to/${topThree[0].model}.jpg`} alt={`${topThree[0].model} image`} />
+        <img src={topThree[0].image} alt={`${topThree[0].model} image`} />
         <h4>{topThree[0].model}</h4>
       </div>
       <div className="podium-item silver">
         <h3>2nd</h3>
-        <img src={`path/to/${topThree[1].model}.jpg`} alt={`${topThree[1].model} image`} />
+        <img src={topThree[1].image} alt={`${topThree[1].model} image`} />
         <h4>{topThree[1].model}</h4>
       </div>
       <div className="podium-item bronze">
         <h3>3rd</h3>
-        <img src={`path/to/${topThree[2].model}.jpg`} alt={`${topThree[2].model} image`} />
+        <img src={topThree[2].image} alt={`${topThree[2].model} image`} />
         <h4>{topThree[2].model}</h4>
       </div>
     </div>
