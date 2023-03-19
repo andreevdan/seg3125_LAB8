26dc8e320442c5af2d6646003cee4580abfb7584
Azan Mubashar <71412049+azan-m@users.noreply.github.com>
2023-03-19 15:02:05 -0400

    Made navbar functional

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 import Navbar from './Navbar';
+import Results from './Results';
+import Vote from './Vote';
 
 const App = () => {
   return (
-    <div>
-      <Navbar/>
-      <Home/>
-    </div>
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Routes>
+            <Route exact path='/' element={<Home/>}/>
+            <Route path='/about' element={<About/>} />
+            <Route path='/result' element={<Results/>} />
+            <Route path='/vote' element={<Vote/>} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     
   );
 };
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import './Navbar.css'; // import the CSS file
-import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <ul className="navbar-list">
-        <li><a href="#" className="navbar-link">Home</a></li>
-        <li><a href="#" className="navbar-link">Home</a></li>
-        <li><a href="#" className="navbar-link">Vote</a></li>
-        <li><a href="#" className="navbar-link">Results</a></li>
+        <li><a href='/' className="navbar-link">Home</a></li>
+        <li><a href='/about' className="navbar-link">About</a></li>
+        <li><a href='/vote' className="navbar-link">Vote</a></li>
+        <li><a href='/result' className="navbar-link">Results</a></li>
       </ul>
     </nav>
   );
