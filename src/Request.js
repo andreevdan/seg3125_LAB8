80b9cc6339049ec170598293efb57e9b7f79cb2f
evan-z-s <ezegl014@uottawa.ca>
2023-03-23 12:57:23 -0400

    Centered Request Page

--- src/Request.js
+++ src/Request.js
@@ -6,21 +6,22 @@ const Request = () => {
   return (
     <div className="request-container">
       <div className='help'><HelpButton/></div>
-      <h1>Request a Car</h1>
-      <form>
+      <h1 style={{marginTop:50,textAlign:'center'}}>Request a Car</h1>
+      <form style={{textAlign:'center'}}>
         <label for="image">Upload an car image: </label>
-        <input type="file" id="image" name="image" accept="image" />
+        <br></br>
+        <input style = {{marginLeft:'70px'}} type="file" id="image" name="image" accept="image" />
         <p>Car Description:</p>
         <label for="description"></label>
-        <textarea id="description" name="description" rows="5" cols="50"></textarea>
+        <textarea style = {{width:'70%'}} id="description" name="description" rows="5" cols="50"></textarea>
         <br/>
         <br/>
         <p>Additonal Comments:</p>
         <label for="comments"></label>
-        <textarea id="comments" name="comments" rows="5" cols="50"></textarea>
+        <textarea style = {{width:'70%'}} id="comments" name="comments" rows="5" cols="50"></textarea>
         <br/>
         <br/>
-        <input type="submit" value="Submit" className='submit-button'></input>
+        <input type="submit" value="Submit" className='request-button'></input>
       </form>
     </div>
   );
